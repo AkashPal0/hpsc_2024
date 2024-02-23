d7fdaff2045f6ac27382ddaeaaeb41f291a91cea
Zeekersky <pal82087@gmail.com>
2024-02-23 18:57:21 +0530

    Loop of boolean code changes

--- Fortran/basics/boolean1.f90
+++ Fortran/basics/boolean1.f90
@@ -5,15 +5,20 @@ program boolean1
     logical :: ever_zero
 
     ever_zero = .false.
-    do i=1,10
+    do 69 i=1,10
         k = 3*i - 1
         ever_zero = (ever_zero .or. (k == 0))
-        enddo
+    69    continue
+
+    ! do i=1,10
+    !     k = 3*i - 1
+    !     ever_zero = (ever_zero .or. (k == 0))
+    ! enddo
 
     if (ever_zero) then
-        print *, "3*i - 1 takes the value 0 for some i"
+        print *, "3*i - 1 takes the value 0 for some 10"
     else
-        print *, "3*i - 1 is never 0 for i tested"
+        print *, "3*i - 1 is never 0 for 10 tested"
     endif
 
 end program boolean1
